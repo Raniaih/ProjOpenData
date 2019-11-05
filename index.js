7fa8e26a11d628c55f8a224505171c1493feda24
BenMlahA <35455977+BenMlahA@users.noreply.github.com>
2019-11-05 10:43:01 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var hostname = 'localhost';
+var port = 3000 ; 
+ 
+var app = express();
+
+app.listen(port, hostname, function(){
+    console.log("Le serveur tourne sur http://"+ hostname +":"+ port +"\n");
+
+});
