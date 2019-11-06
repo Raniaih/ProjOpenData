c131939ec5f6a2a53d43a6168642edb6ec48ecbb
BenMlahA <35455977+BenMlahA@users.noreply.github.com>
2019-11-06 18:57:52 +0100

    c

--- index.js
+++ index.js
@@ -3,62 +3,11 @@ var hostname = 'localhost';
 var port = 3000 ; 
 const fetch = require('node-fetch');
 var path = require('path');
+var jsonToCsv = require('convert-json-to-csv');
  
 var app = express();
 
 
-/* const getTodosAsync = async function () {
-    const response = await fetch('https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=Paris', {
-        
-        method: "GET",
-        headers: {
-            "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
-            "x-rapidapi-key": "NNMylDnsEhmshXgwnJKxRL7A75t4p1IEfvojsnPaM5k3D7gd6G",
-            'Content-Type': 'application/json'
-        },
-    });
-    const jsonData = await response.json().then(data => {return data});
-    return  jsonData;
-} */
-/* async function apiGetAll () {
-        var location = 'Paris';
-    // here we tell the engine to PAUSE EXECUTION and wait for a response
-    // once it has a reponse, assign it to the variable 'resp' and continue
-         const resp = await fetch('https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location='+location, {
-        
-            method: "GET",
-            headers: {
-                "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
-                "x-rapidapi-key": "NNMylDnsEhmshXgwnJKxRL7A75t4p1IEfvojsnPaM5k3D7gd6G",
-                'Content-Type': 'application/json'
-            },
-        }).then(res => res.json());
-
-        const data = await resp;
-        
-
-    // this code will NOT run until 'resp' has been assigned
-    // and then the program will PAUSE until it gets a reponse
-        return data ;
-   }  */
-/* function fetchDevruJson(location = 'Paris'){
-    
-    return fetch('https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location='+location, {
-        
-            method: "GET",
-            headers: {
-                "x-rapidapi-host": "devru-latitude-longitude-find-v1.p.rapidapi.com",
-                "x-rapidapi-key": "NNMylDnsEhmshXgwnJKxRL7A75t4p1IEfvojsnPaM5k3D7gd6G",
-                'Content-Type': 'application/json'
-            },
-        })
-        .then(res => res.json())
-        .then(json => data = json['Results']);
-
-        
-
-}
- */
 function fetchCountryJson(location = 'Tunisia'){
     
     return fetch('https://restcountries.eu/rest/v2/name/'+location+'?fullText=true'    , {
@@ -76,7 +25,6 @@ function fetchCountryJson(location = 'Tunisia'){
 
 };
  
-fetchCountryJson().then(data => console.log(data));
 
 
 function fetchWeatherJson(latlng){
@@ -104,25 +52,7 @@ function fetchWeatherJson(latlng){
 
 };
 
-//fetchDevruJson('Alger').then(data => fetchWeatherJson(data))
 
-//fetchDevruJson('Alger').then(data => console.log(data))
-
-//fetchWeatherJson(fetchDevruJson().then(data => {return data})).then(data => console.log(data));
-
-//fetchDevruJson().then(data => console.log(data));
-
-//data2 = fetchDevruJson(location= 'Paris');
-//console.log(getTodosAsync)       
-
-//data2= apiGetAll()
-
-
-
-
-//const data1 = await getTodosAsync();
-
-//console.log(data1);
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/accueil.html'))
 });
@@ -151,8 +81,10 @@ app.get('/villes', function(req,res) {
 
 app.get('/villes/:ville', function(req, res){
 
-    fetchCountryJson(req.params.ville).then(data => fetchWeatherJson(data.latlng).then(data => res.send(console.log(data['daily']['data'])))) ;
+    fetchCountryJson(req.params.ville).then(data => fetchWeatherJson(data.latlng).then(data => console.log(data))) ;
 }) ;
+
+
 app.listen(port, hostname, function(){
     console.log("Le serveur tourne sur http://"+ hostname +":"+ port +"\n")
 });
